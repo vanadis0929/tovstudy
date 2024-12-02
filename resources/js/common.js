459a5446d3bffb4518fd953a9c1e4c3642431602
bo.kim <bo.kim@tovsoft.com>
2024-12-02 12:03:59 +0900

    aside 포커스 제외 완료

--- resources/js/common.js
+++ resources/js/common.js
@@ -16,10 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
     event.currentTarget.setAttribute("disabled", "disabled");
 
     document.querySelectorAll(`.ly_main article[id]`).forEach((current) => {
-      current.classList.add("hidden");
+      // current.classList.add("hidden");
+      current.setAttribute("hidden", "hidden");
+      current.setAttribute("tabindex", "-1");
     });
 
     document.querySelector(`${event.currentTarget.dataset.href}`).classList.remove("hidden");
+    document.querySelector(`${event.currentTarget.dataset.href}`).removeAttribute("hidden");
+    document.querySelector(`${event.currentTarget.dataset.href}`).setAttribute("tabindex", "0");
+    document.querySelector(`${event.currentTarget.dataset.href}`).focus();
   };
 
   document.querySelectorAll(".bl_lnb button").forEach((current) => {
